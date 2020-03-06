3eed5361b6a4225bac822b2753bff1f4eec6bd36
Ravi Botla <ravikiran.botla@gmail.com>
2020-03-05 21:17:54 -0800

    updated

--- kafka/producer-consumer/python/tweet_consumer.py
+++ kafka/producer-consumer/python/tweet_consumer.py
@@ -8,7 +8,13 @@ config.read(r'./config.cfg')
 kafka_server = config.get('kafka', 'server')
 kafka_topic = config.get('kafka', 'topic')
 
+tweet_download_file = config.get('consumer_file', 'file_name')
+
+fileHandle = open(tweet_download_file, "a+")
+
 consumer = KafkaConsumer(bootstrap_servers=kafka_server, auto_offset_reset='latest', value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 consumer.subscribe([kafka_topic])
 for message in consumer :
-  print(message)
+  fileHandle.write(message.text)
+
+fileHandle.close()
