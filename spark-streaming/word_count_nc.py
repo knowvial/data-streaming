febed92dde6ccf05cf1ce19e0f5238b700ba5253
Ravi Botla <ravikiran.botla@gmail.com>
2020-03-07 01:29:08 -0800

    Sat Mar  7 01:29:08 PST 2020

--- spark-streaming/word_count_nc.py
+++ spark-streaming/word_count_nc.py
@@ -1,33 +1,64 @@
+import sys
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 
-spark = SparkSession \
-    .builder \
-    .appName("StructuredNetworkWordCount") \
-    .getOrCreate()
+if __name__ == "__main__":
+    
+    if len(sys.argv) != 3:
+        print("Usage: spark-submit m01_demo01_netcat.py <hostname> <port>", file=sys.stderr)
+        exit(-1)
 
-lines = spark \
-    .readStream \
-    .format("socket") \
-    .option("host", "localhost") \
-    .option("port", 9999) \
-    .load()
 
-# Split the lines into words
-words = lines.select(
-   explode(
-       split(lines.value, " ")
-   ).alias("word")
-)
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    
+    spark = SparkSession\
+        .builder\
+        .appName("NetcatWordCount")\
+        .getOrCreate()
 
-# Generate running word count
-wordCounts = words.groupBy("word").count()
+    spark.sparkContext.setLogLevel("ERROR")
 
-query = wordCounts \
-    .writeStream \
-    .outputMode("complete") \
-    .format("console") \
-    .start()
+    # Create DataFrame representing the stream of input lines from connection to host:port
+    # We're reading from the socket on the port where netcat is listening
+    lines = spark\
+        .readStream\
+        .format('socket')\
+        .option('host', host)\
+        .option('port', port)\
+        .load()
 
-query.awaitTermination()
+
+    # Split the lines into words
+    # Explode turns each item in an array into a separate row
+    # Alias sets the name of the column for the words
+    # The result - each word of input is a row in a table with one column named "word"
+    words = lines.select(
+        explode(
+            split(lines.value, ' ')
+        ).alias('word')
+    )
+
+
+    # Generate running word count
+    wordCounts = words.groupBy('word')\
+                      .count()
+
+
+    # Start running the query that prints the running counts to the console
+    # Running in "complete" mode ensures that any operation uses ALL data 
+    # - from previous and current batch 
+    # The call to format sets where the stream is written to
+    query = wordCounts.writeStream\
+                      .outputMode('complete')\
+                      .format('console')\
+                      .start()
+
+    query.awaitTermination()
+
+
+
+
+
+
