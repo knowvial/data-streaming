525f8a84c45afdfd10f08b304af33db767c94a59
Ravi Botla <ravikiran.botla@gmail.com>
2020-03-08 08:22:59 -0700

    updated

--- kafka/producer-consumer/python/tweet_consumer.py
+++ kafka/producer-consumer/python/tweet_consumer.py
@@ -18,6 +18,7 @@ consumer.subscribe([kafka_topic])
 for message in consumer :
   try:
     #msg = message.value.get("user","").get("name", "") + "\t" + message.value.get("created_at","") + "\t" + message.value.get("text", "") + "\t" + message.value.get("retweet_count","")
+    print(message.value)
     fileHandle.write(message.value + "\n")
   except:
     print("Unexpected error:", sys.exc_info()[0])
