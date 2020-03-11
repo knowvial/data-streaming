6f1391582c195b1e7bc7b5524fc88d5e76fec464
Ravi Botla <ravikiran.botla@gmail.com>
2020-03-11 16:31:01 -0700

    Wed Mar 11 16:31:01 PDT 2020

--- spark-streaming/windows.py
+++ spark-streaming/windows.py
@@ -2,6 +2,10 @@ import sys
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
+from pyspark.sql.functions import window
+from pyspark.sql.functions import udf
+import time
+import datetime
 
 if __name__ == "__main__":
     
@@ -36,18 +40,47 @@ if __name__ == "__main__":
     )
     nums = nums.selectExpr("cast(num as int) num")
 
+    def add_timestamp():
+      ts = time.time()
+      timestamp = datatime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H-%M-%S')
+      return timestamp
+
+    add_timestamp_udf = udf(add_timestamp, StringType())
+    numsTs = nums.withColumn("timestamp", add_timestamp_udf())
+
     if option == 'stateless':
-        query = nums.filter(nums.num > 65)\
-                  .writeStream\
-                  .outputMode('append')\
-                  .trigger(processingTime="10 seconds")\
-                  .format('console')\
-                  .start()
+      query = nums.filter(nums.num > 65)\
+                .writeStream\
+                .outputMode('append')\
+                .trigger(processingTime="10 seconds")\
+                .format('console')\
+                .start()
     elif option == 'rolling':
-        print('rolling')
+      print('rolling')
     elif option == 'tumble':
-        print('tumble')
+      windowedCount = numsTs.groupBy(window(numsTs.timestamp, "60 seconds", "60 seconds"))\
+                          .agg({"num": "sum"})\
+                          .withColumnRenamed("sum(num)", "TotalVehicles")\
+                          .orderBy('TotalVehicles', ascending=False)
+      
+      query = windowedCount.writeStream\
+                  .outputMode("complete")\
+                  .format("console")\
+                  .option("truncate", "false")\
+                  .start()\
+                  .awaitTermination()
+
     elif option == 'sliding':
-        print('sliding')
+      numsTs.groupBy(window(numsTs.timestamp, "60 seconds", "30 seconds"))\
+        .agg({"num": "sum"})\
+        .withColumnRenamed("sum(num)", "TotalVehicles")\
+        .orderBy('TotalVehicles', ascending=False)
+
+      query = windowedCount.writeStream\
+                  .outputMode("complete")\
+                  .format("console")\
+                  .option("truncate", "false")\
+                  .start()\
+                  .awaitTermination()
 
     query.awaitTermination()
