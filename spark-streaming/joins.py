2f2b90b723deda130c477b5da50c6a9b8a7355df
Ravi Botla <ravikiran.botla@gmail.com>
2020-03-11 18:07:00 -0700

    Wed Mar 11 18:07:00 PDT 2020

---
+++ spark-streaming/joins.py
@@ -0,0 +1,65 @@
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession
+
+
+if __name__ == "__main__":
+
+    sparkSession = SparkSession.builder.master("local")\
+                              .appName("SparkStreamingSQLQuery")\
+                              .getOrCreate()
+
+    sparkSession.sparkContext.setLogLevel("ERROR")
+
+    schema = StructType([StructField("product", StringType(), True),
+                         StructField("city", StringType(), True),
+                         StructField("state", StringType(), True),
+                         StructField("country", StringType(), True),
+                         StructField("sales", StringType(), True)
+                         ])
+
+    fileStreamDF = sparkSession.readStream\
+                               .option("header", "false")\
+                               .option("maxFilesPerTrigger", 2)\
+                               .schema(schema)\
+                               .csv("./datasets/droplocation")
+
+    headsSchema = StructType([StructField("regional_head", StringType(), True),
+                         StructField("city", StringType(), True),
+                         StructField("state", StringType(), True),
+                         StructField("country", StringType(), True)
+                         ])
+
+    headsDF = sparkSession.read\
+                    .format("csv")\
+                    .option("header", "false")\
+                    .schema(headsSchema)\
+                    .load("./datasets/heads.csv")
+
+
+    # Registering Table
+    # Create a view which can later be queried like a table
+    fileStreamDF.createOrReplaceTempView("SalesData")
+
+    categoryDF = sparkSession.sql("SELECT city, sales \
+                                    FROM SalesData \
+                                    WHERE state = 'California'")
+
+    joinDF = headsDF.join( categoryDF, ["city", "state", "country"] )
+
+    sales = joinDF.groupBy("regional_head")\
+                  .agg({"sales": "sum"})\
+                  .withColumnRenamed("sum(sales)", "tot_sales")\
+                  .orderBy("tot_sales", ascending=False)
+
+    # Write out our dataframe to the console
+    query = sales.writeStream\
+                      .outputMode("complete")\
+                      .format("console")\
+                      .option("truncate", "false")\
+                      .option("numRows", 30)\
+                      .start()\
+                      .awaitTermination()
+
+
+
+
