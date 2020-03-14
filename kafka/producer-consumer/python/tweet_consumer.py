7d913e444f0cbde388cde5f257b9aaa0494f994e
Ravi Botla <ravikiran.botla@gmail.com>
2020-03-13 23:07:37 -0700

    Fri Mar 13 23:07:37 PDT 2020

--- kafka/producer-consumer/python/tweet_consumer.py
+++ kafka/producer-consumer/python/tweet_consumer.py
@@ -13,7 +13,7 @@ tweet_download_file = config.get('consumer_file', 'file_name')
 
 fileHandle = open(tweet_download_file, "a+")
 
-consumer = KafkaConsumer(bootstrap_servers=kafka_server, group_id='app1', auto_offset_reset='earliest')
+consumer = KafkaConsumer(bootstrap_servers=kafka_server, group_id='app1', auto_offset_reset='latest')
 consumer.subscribe([kafka_topic])
 for message in consumer:
   try:
