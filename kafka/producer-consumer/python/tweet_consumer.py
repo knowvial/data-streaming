e83f33d87a74adeedafa5d3eb74260f4257b8d93
Ravi Botla <ravikiran.botla@gmail.com>
2020-03-13 23:02:31 -0700

    Fri Mar 13 23:02:31 PDT 2020

--- kafka/producer-consumer/python/tweet_consumer.py
+++ kafka/producer-consumer/python/tweet_consumer.py
@@ -15,7 +15,7 @@ fileHandle = open(tweet_download_file, "a+")
 
 consumer = KafkaConsumer(bootstrap_servers=kafka_server, group_id='app1', auto_offset_reset='earliest', value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 consumer.subscribe([kafka_topic])
-for message in consumer :
+for message in consumer:
   try:
     #msg = message.value.get("user","").get("name", "") + "\t" + message.value.get("created_at","") + "\t" + message.value.get("text", "") + "\t" + message.value.get("retweet_count","")
     print(message.value)
