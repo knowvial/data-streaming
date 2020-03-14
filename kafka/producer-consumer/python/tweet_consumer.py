2a176752edf88f6ffd5adb65ec3fc43a7db67bc9
Ravi Botla <ravikiran.botla@gmail.com>
2020-03-13 23:17:31 -0700

    Fri Mar 13 23:17:31 PDT 2020

--- kafka/producer-consumer/python/tweet_consumer.py
+++ kafka/producer-consumer/python/tweet_consumer.py
@@ -13,12 +13,12 @@ tweet_download_file = config.get('consumer_file', 'file_name')
 
 fileHandle = open(tweet_download_file, "a+")
 
-consumer = KafkaConsumer(bootstrap_servers=kafka_server, group_id='app1', auto_offset_reset='latest', value_deserializer=lambda m: m.decode('utf-8'))
+consumer = KafkaConsumer(bootstrap_servers=kafka_server, group_id='app1', auto_offset_reset='latest')
 consumer.subscribe([kafka_topic])
 for message in consumer:
   try:
     #msg = message.value.get("user","").get("name", "") + "\t" + message.value.get("created_at","") + "\t" + message.value.get("text", "") + "\t" + message.value.get("retweet_count","")
-    print(message.value)
+    print(type(message.value))
     fileHandle.write(message.value + "\n")
   except:
     print("Unexpected error:", sys.exc_info()[0])
